c559260a75af2879bbf8d7d438bdc68cd90fd592
SergioRM <sergio93rm@gmail.com>
2022-04-06 11:05:17 +0200

    Contenido de la pagina y su css

---
+++ gshtml5/resources/views/content.blade.php
@@ -0,0 +1,136 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tarjetas Descripción y TagName</title>
+    <link rel="stylesheet" href="../bootstrap-5.0.2-dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/BS-tarjeta-tagname/style.css">
+    <script src="../bootstrap-5.0.2-dist/js/bootstrap.min.js"></script>
+</head>
+<body>
+    <header class="navbar__1 navbar-expand-sm navbar-dark text-light">
+        <div class="container-fluid d-flex justify-content-between align-content-center align-items-center">
+            <div class="d-flex justify-content-center align-content-center align-items-center">
+                <div class="bottones">
+                    <a id="abrir" class="abrir-cerrar" href="javascript:void(0)" onclick="mostrar()">
+                        <img src="./../sources/img/iconmenu.png" alt="iconmenu">
+                    </a>
+                    <a id="cerrar" class="abrir-cerrar" href="javascript:void(0)" onclick="ocultar()">
+                        <img src="./../sources/img/iconmenu.png" alt="iconmenu">
+                    </a>
+                </div>
+                <h1>Guía Suprema - HTML5</h1>
+            </div>
+            <form class="d-flex">
+                <input class="form-control me-2" type="text" placeholder="Search...">
+                <button class="btn search-button" type="button">Search</button>
+            </form>
+        </div>
+    </header>
+    <nav class="navbar navbar-expand-lg ">
+        <div class="container-fluid">
+          <a class="nav-link link-warning" href="#">Etiqueta</a>
+          <button class="navbar-toggler bg-warning" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a class="nav-link link-warning active" aria-current="page" href="#descripcion">Descripción</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link link-warning" href="#atributos"># Atributos</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link link-warning" href="#ejemplos">Ejemplos</a>
+              </li>
+                
+            </ul>
+           
+          </div>
+        </div>
+      </nav>
+<main>
+    <article class="container">
+    
+
+
+    <section class="card-wrapper " >
+        <a name="descripcion"></a>
+        <div class="card-header">Etiqueta</div>
+        <div class="card-body">
+        <h5 class="card-title">Special title treatment</h5>
+        <p class="card-text">With supporting text below as a natural lead-in to additional content.
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Numquam amet quia totam sed quibusdam eos sint illum corrupti animi repudiandae. Blanditiis repellat ex quis reprehenderit fugiat illo explicabo iste hic?
+        </p>
+        <p>Lorem fistrum ese que llega no te digo trigo por no llamarte Rodrigor llevame al sircoo se calle ustée. Amatomaa jarl va usté muy cargadoo qué dise usteer a gramenawer no te digo trigo por no llamarte Rodrigor.
+        
+        Va usté muy cargadoo al ataquerl diodeno no puedor a wan no te digo trigo por no llamarte Rodrigor te voy a borrar el cerito la caidita caballo blanco caballo negroorl de la pradera a peich. Pecador torpedo sexuarl te voy a borrar el cerito.</p>
+        <p>Va usté muy cargadoo al ataquerl diodeno no puedor a wan no te digo trigo "lazarillo"</p>
+        </div>
+        
+    </section>
+    <hr>
+    <section class="card-wrapper " >
+        <a name="atributos"></a>
+
+        <div class="card-header"># Atributos</div>
+        <div class="card-body">
+        <h5 class="card-title">Special title treatment</h5>
+        <p class="card-text">With supporting text below as a natural lead-in to additional content.
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Numquam amet quia totam sed quibus
+            dam eos sint illum corrupti animi repudiandae. Blanditiis repellat ex quis reprehenderit fugiat i
+            llo explicabo iste hic?
+
+        </p>
+        <ul>
+            <li>atributo Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam commodi numquam enim, laudantium voluptates pariatur provident sed non soluta voluptas cum reiciendis temporibus similique! Aliquam pariatur voluptatum id fugiat deserunt.</li>
+            <li>atributo Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam commodi numquam enim, laudantium voluptates pariatur provident sed non soluta voluptas cum reiciendis temporibus similique! Aliquam pariatur voluptatum id fugiat deserunt.</li>
+            <li>atributo Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam commodi numquam enim, laudantium voluptates pariatur provident sed non soluta voluptas cum reiciendis temporibus similique! Aliquam pariatur voluptatum id fugiat deserunt.</li>
+            <li>atributo Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam commodi numquam enim, laudantium voluptates pariatur provident sed non soluta voluptas cum reiciendis temporibus similique! Aliquam pariatur voluptatum id fugiat deserunt.</li>
+            <li>atributo Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam commodi numquam enim, laudantium voluptates pariatur provident sed non soluta voluptas cum reiciendis temporibus similique! Aliquam pariatur voluptatum id fugiat deserunt.</li>
+            <li>atributo Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam commodi numquam enim, laudantium voluptates pariatur provident sed non soluta voluptas cum reiciendis temporibus similique! Aliquam pariatur voluptatum id fugiat deserunt.</li>
+        </ul>    
+        </div>
+        
+    </section>
+    <hr>
+    <section class="card-wrapper " >
+        <a name="ejemplos"></a>
+
+        <div class="card-header">Ejemplo</div>
+        <div class="card-body">
+        <h5 class="card-title">Special title treatment</h5>
+        <p class="card-text">With supporting text below as a natural lead-in to additional content.
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Numquam amet quia totam sed quibusdam eos sint illum corrupti animi repudiandae. Blanditiis repellat ex quis reprehenderit fugiat illo explicabo iste hic?
+        </p>
+        <ul>
+            <li>texto ejemplo</li>
+            <li>texto ejemplo</li>
+            <li>texto ejemplo</li>
+            <li>texto ejemplo</li>
+            <li>texto ejemplo</li>
+            <li>texto ejemplo</li>
+            <li>texto ejemplo</li>
+            <li>texto ejemplo</li>
+            <li>texto ejemplo</li>
+            <li>texto ejemplo</li>
+            <li>texto ejemplo</li>
+            <li>texto ejemplo</li>
+            <li>Lorem fistrum ese que llega no te digo trigo por no llamarte Rodrigor llevame al sircoo se calle ustée. Amatomaa jarl va usté muy cargadoo qué dise usteer a gramenawer no te digo trigo por no llamarte Rodrigor.</li>
+            <li>Lorem fistrum ese que llega no te digo trigo por no llamarte Rodrigor llevame al sircoo se calle ustée. Amatomaa jarl va usté muy cargadoo qué dise usteer a gramenawer no te digo trigo por no llamarte Rodrigor.</li>
+            <li>Lorem fistrum ese que llega no te digo trigo por no llamarte Rodrigor llevame al sircoo se calle ustée. Amatomaa jarl va usté muy cargadoo qué dise usteer a gramenawer no te digo trigo por no llamarte Rodrigor.</li>
+            <li>Camorrillo</li>
+        </ul>
+        
+        </div>
+        
+    </section>
+        
+    </article>    
+</main>
+  
+</body>
+</html>
