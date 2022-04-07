deff89e6edb8e1e08fbf1cb6bb4de993d66cf8fb
JuanH <92516277+jehrx@users.noreply.github.com>
2022-04-07 19:25:17 +0200

    Logo secundario

--- gshtml5/public/js/navbar.js
+++ gshtml5/public/js/navbar.js
@@ -3,7 +3,7 @@ window.addEventListener("resize",resize);
 
 function scroll(){
     if (window.scrollY > 200) {
-        document.getElementById("logo").innerHTML = '<div class="suprema  mt-5 mb-5 h">H<span class="tml">TML</span><span class="cinco">5</span> <span class="g">G</span> <span class="s">S</span> </div>';
+        document.getElementById("logo").innerHTML = '<div class="  mt-4 mb-4 h">H<span class="tml">TML</span><span class="cinco">5</span> <span class="g">G</span> <span class="suprema s">S</span> </div>';
     }
     if (window.scrollY <= 0 && window.innerWidth >= 576){
         document.getElementById("logo").innerHTML = '<p class="guia">Guía</p><p class="suprema"><span>S<span>uprema </p><p class="html5">HTML5</p>';
@@ -12,7 +12,7 @@ function scroll(){
 
 function resize(){
     if (window.innerWidth < 576) {
-        document.getElementById("logo").innerHTML = '<div class="suprema  mt-5 mb-5 h">H<span class="tml">TML</span><span class="cinco">5</span> <span class="g">G</span> <span class="s">S</span> </div>';
+        document.getElementById("logo").innerHTML = '<div class=" mt-4 mb-4 h">H<span class="tml">TML</span><span class="cinco">5</span> <span class="g">G</span> <span class=" suprema s">S</span> </div>';
     }
     if (window.innerWidth >= 576){
         document.getElementById("logo").innerHTML = '<p class="guia">Guía</p><p class="suprema"><span>S<span>uprema </p> <p class="html5">HTML5 </p>';
