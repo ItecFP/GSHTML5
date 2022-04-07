9ca6b4b164a7a8b3c6d1c45b3666cdd58e5a9e21
Ruben <perea7r@gmail.com>
2022-04-07 22:25:11 +0200

    Cambio neon

--- gshtml5/public/js/navbar.js
+++ gshtml5/public/js/navbar.js
@@ -15,10 +15,6 @@ function resize(){
         document.getElementById("logo").innerHTML = '<div class="suprema  mt-5 mb-5 h">H<span class="tml">TML</span><span class="cinco">5</span> <span class="g">G</span> <span class="s">S</span> </div>';
     }
     if (window.innerWidth >= 576){
-<<<<<<< HEAD
         document.getElementById("logo").innerHTML = '<p class="guia">Guía</p><p class="suprema fall"><span class="parpadear">S</span>uprema </p><p class="html5">HTML5</p>';
-=======
-        document.getElementById("logo").innerHTML = '<p class="guia">Guía</p><p class="suprema"><span>S<span>uprema </p> <p class="html5">HTML5 </p>';
->>>>>>> cd8516c6df5e64c254280b2b562631ab14990c55
     }
 }
