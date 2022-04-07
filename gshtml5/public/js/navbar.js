22a776d0c434a496f1e1f9f37745187d10b70fd7
JuanH <92516277+jehrx@users.noreply.github.com>
2022-04-07 14:13:14 +0200

    más cambios en el logo secundario

--- gshtml5/public/js/navbar.js
+++ gshtml5/public/js/navbar.js
@@ -3,7 +3,7 @@ window.addEventListener("resize",resize);
 
 function scroll(){
     if (window.scrollY > 200) {
-        document.getElementById("logo").innerHTML = '<p class="suprema h">H<span class="tml">TML</span><span class="cinco">5</span></p>';
+        document.getElementById("logo").innerHTML = '<p class="suprema  mt-5 mb-5 h">H<span class="tml">TML</span><span class="cinco">5</span> <span class="g">G</span> <span class="s">S</span> </p>';
     }
     if (window.scrollY <= 0 && window.innerWidth >= 576){
         document.getElementById("logo").innerHTML = '<p class="guia">Guía</p><p class="suprema"><span>S<span>uprema </p><p class="html5">HTML5</p>';
@@ -12,9 +12,9 @@ function scroll(){
 
 function resize(){
     if (window.innerWidth < 576) {
-        document.getElementById("logo").innerHTML = '<p class="suprema h">H<span class="tml">TML</span><span class="cinco">5</span></p>';
+        document.getElementById("logo").innerHTML = '<p class="suprema  mt-5 mb-5 h">H<span class="tml">TML</span><span class="cinco">5</span> <span class="g">G</span> <span class="s">S</span> </p>';
     }
     if (window.innerWidth >= 576){
-        document.getElementById("logo").innerHTML = '<p class="guia">Guía</p><p class="suprema"><span>S<span>uprema </p><p class="html5">HTML5</p>';
+        document.getElementById("logo").innerHTML = '<p class="guia">Guía</p><p class="suprema"><span>S<span>uprema </p> <p class="html5">HTML5 </p>';
     }
 }
