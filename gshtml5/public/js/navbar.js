c0e58f9a07ddb066f24faba5ea838e813cdfcac4
JuanH <92516277+jehrx@users.noreply.github.com>
2022-04-07 20:31:42 +0200

    Saltitos palabras guía y html en el logo principal, para sensación de empuje de la palabra suprema a ambas palabras

--- gshtml5/public/js/navbar.js
+++ gshtml5/public/js/navbar.js
@@ -3,22 +3,18 @@ window.addEventListener("resize",resize);
 
 function scroll(){
     if (window.scrollY > 200) {
-        document.getElementById("logo").innerHTML = '<div class="  mt-4 mb-4 h">H<span class="tml">TML</span><span class="cinco">5</span> <span class="g">G</span> <span class="suprema s">S</span> </div>';
+        document.getElementById("logo").innerHTML = '<div class="  h">H<span class="tml">TML</span><span class="cinco">5</span> <span class="g">G</span> <span class="suprema s">S</span> </div>';
     }
     if (window.scrollY <= 0 && window.innerWidth >= 576){
-        document.getElementById("logo").innerHTML = '<p class="guia">Guía</p><p class="suprema fall"><span class="parpadear">S</span>uprema </p><p class="html5">HTML5</p>';
+        document.getElementById("logo").innerHTML = '<p class="fall2 guia">Guía</p><p class="suprema fall"><span class="parpadear">S</span>uprema </p><p class="fall3 html5">HTML5</p>';
     }
 }
 
 function resize(){
     if (window.innerWidth < 576) {
-        document.getElementById("logo").innerHTML = '<div class=" mt-4 mb-4 h">H<span class="tml">TML</span><span class="cinco">5</span> <span class="g">G</span> <span class=" suprema s">S</span> </div>';
+        document.getElementById("logo").innerHTML = '<div class=" h">H<span class="tml">TML</span><span class="cinco">5</span> <span class="g">G</span> <span class=" suprema s">S</span> </div>';
     }
     if (window.innerWidth >= 576){
-<<<<<<< HEAD
-        document.getElementById("logo").innerHTML = '<p class="guia">Guía</p><p class="suprema fall"><span class="parpadear">S</span>uprema </p><p class="html5">HTML5</p>';
-=======
-        document.getElementById("logo").innerHTML = '<p class="guia">Guía</p><p class="suprema"><span>S<span>uprema </p> <p class="html5">HTML5 </p>';
->>>>>>> cd8516c6df5e64c254280b2b562631ab14990c55
+        document.getElementById("logo").innerHTML = '<p class="fall2 guia">Guía</p><p class="suprema fall"><span class="parpadear">S</span>uprema </p><p class="fall3 html5">HTML5</p>';
     }
 }
