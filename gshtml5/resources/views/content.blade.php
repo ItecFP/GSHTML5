f19b7298151eb28ae9dcc91ac0442776ba822a0d
SergioRM <sergio93rm@gmail.com>
2022-04-07 10:01:37 +0200

    añadidio una ruta para ejemplo, dos ventanas nuevas de contenido fraccionadas

--- gshtml5/resources/views/content.blade.php
+++ gshtml5/resources/views/content.blade.php
@@ -5,8 +5,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tarjetas Descripción y TagName</title>
-    <link rel="stylesheet" href="../bootstrap-5.0.2-dist/css/bootstrap.min.css">
-    <link rel="stylesheet" href="/BS-tarjeta-tagname/style.css">
+    <link rel="stylesheet" href="\bootstrap-5.1.3-dist\css\bootstrap.min.css">
+    <link rel="stylesheet" href="\css\styleContent.css">
     <script src="../bootstrap-5.0.2-dist/js/bootstrap.min.js"></script>
 </head>
 <body>
