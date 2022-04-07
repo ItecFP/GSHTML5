21badb3e8269b4e66a5176e3efc0583a90d7f892
Ruben <perea7r@gmail.com>
2022-04-07 11:42:20 +0200

    Cambios topbar

--- gshtml5/public/js/sidebar.js
+++ gshtml5/public/js/sidebar.js
@@ -5,21 +5,3 @@ function mostrar() {
     }else{
         sidebar.classList.add("abierto");
     }*/
-    document.getElementById("sidebar").
-    document.getElementById("sidebar").style.width = "25vh";
-    document.getElementById("sidebar").style.paddingLeft = "15px";
-    document.getElementById("sidebar").style.paddingRigth = "15px";
-    document.getElementById("contenido").style.marginLeft = "25vh";
-    document.getElementById("abrir").style.display = "none";
-    document.getElementById("cerrar").style.display = "inline";
-}
-
-function ocultar() {
-    document.getElementById("sidebar").style.width = "0";
-    document.getElementById("sidebar").style.paddingLeft = "0";
-    document.getElementById("sidebar").style.paddingRigth = "0";
-    document.getElementById("contenido").style.marginLeft = "0";
-    document.getElementById("abrir").style.display = "inline";
-    document.getElementById("sidebar").style.display = "inline";
-    document.getElementById("cerrar").style.display = "none";
-}
