0525e959efdfcccb5a06ab3c752e07905690e82b
Ruben <perea7r@gmail.com>
2022-04-07 14:23:53 +0200

    Cambio

--- gshtml5/public/js/navbar.js
+++ gshtml5/public/js/navbar.js
@@ -6,7 +6,7 @@ function shrink(){
         document.getElementById("logo").innerHTML = '<p class="suprema mt-4 mb-4">G<span>S</span>HTML5</p>';
     }
     if (window.scrollY <= 0 && window.innerWidth >= 576){
-        document.getElementById("logo").innerHTML = '<p class="guia">Guía</p><p class="suprema"><span>S<span>uprema </p><p class="html5">HTML5</p>';
+        document.getElementById("logo").innerHTML = '<p class="guia">Guía</p><p class="suprema fall"><span>S<span>uprema </p><p class="html5">HTML5</p>';
     }
 }
 
@@ -15,6 +15,6 @@ function resize(){
         document.getElementById("logo").innerHTML = '<p class="suprema mt-4 mb-4">G<span>S</span>HTML5</p>';
     }
     if (window.innerWidth >= 576){
-        document.getElementById("logo").innerHTML = '<p class="guia">Guía</p><p class="suprema"><span>S<span>uprema </p><p class="html5">HTML5</p>';
+        document.getElementById("logo").innerHTML = '<p class="guia">Guía</p><p class="suprema fall"><span>S<span>uprema </p><p class="html5">HTML5</p>';
     }
 }
