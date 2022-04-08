47027e388b47e2b66e8a2ac4067078335f6b21fc
Ruben <perea7r@gmail.com>
2022-04-08 09:17:46 +0200

    Cambios generales al CSS

--- gshtml5/public/js/navbar.js
+++ gshtml5/public/js/navbar.js
@@ -12,13 +12,9 @@ function scroll(){
 
 function resize(){
     if (window.innerWidth < 576) {
-        document.getElementById("logo").innerHTML = '<div class=" h">H<span class="/*fall4 tml">TML</span><span class="cinco">5</span> <span class="g">G</span> <span class=" suprema parpadear2 s">S</span> </div>';
+        document.getElementById("logo").innerHTML = '<div class=" h">H<span class="fall4 tml">TML</span><span class="cinco">5</span> <span class="g">G</span> <span class=" suprema parpadear2 s">S</span> </div>';
     }
     if (window.innerWidth >= 576){
-<<<<<<< HEAD
         document.getElementById("logo").innerHTML = '<p class="guia">Guía</p><p class="suprema fall"><span class="parpadear">S</span>uprema </p><p class="html5">HTML5</p>';
-=======
-        document.getElementById("logo").innerHTML = '<p class="fall2 guia">Guía</p><p class="suprema fall"><span class="parpadear">S</span>uprema </p><p class="fall3 html5">HTML5</p>';
->>>>>>> 9c5fc442f5ab248c7e8243f9e5ba4916bf02e907
     }
 }
