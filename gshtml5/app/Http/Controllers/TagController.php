1f1951162f97011864ca324404737dbcb853fc6e
SergioRM <sergio93rm@gmail.com>
2022-04-08 09:52:50 +0200

    Retocar contenido a mostrar para tagDetail

--- gshtml5/app/Http/Controllers/TagController.php
+++ gshtml5/app/Http/Controllers/TagController.php
@@ -53,25 +53,25 @@ class TagController extends Controller
     //funciones con los datos de las etiquetas
 
     public function tag_a(){
-        $content_etiqueta = file_get_contents('C:\GSHtml5\docker\db\mongodb\data\a.json');
+        $content_etiqueta = file_get_contents('C:\a\docker\db\mongodb\data\a.json');
         $json_en_array_php = json_decode($content_etiqueta, true);
 
         return view("tagDetail",["tagdata"=>$json_en_array_php]);
     }
     public function tag_abbr(){
-        $content_etiqueta = file_get_contents('C:\GSHtml5\docker\db\mongodb\data\abbr.json');
+        $content_etiqueta = file_get_contents('C:\a\docker\db\mongodb\data\abbr.json');
         $json_en_array_php = json_decode($content_etiqueta, true);
 
         return view("tagDetail",["tagdata"=>$json_en_array_php]);
     }
     public function tag_address(){
-        $content_etiqueta = file_get_contents('C:\GSHtml5\docker\db\mongodb\data\address.json');
+        $content_etiqueta = file_get_contents('C:\a\docker\db\mongodb\data\address.json');
         $json_en_array_php = json_decode($content_etiqueta, true);
 
         return view("tagDetail",["tagdata"=>$json_en_array_php]);
     }
     public function tag_del(){
-        $content_etiqueta = file_get_contents('C:\GSHtml5\docker\db\mongodb\data\del.json');
+        $content_etiqueta = file_get_contents('C:\a\docker\db\mongodb\data\del.json');
         $json_en_array_php = json_decode($content_etiqueta, true);
 
         return view("tagDetail",["tagdata"=>$json_en_array_php]);
--- gshtml5/resources/views/tagDetailContent.blade.php
+++ gshtml5/resources/views/tagDetailContent.blade.php
@@ -7,14 +7,17 @@
         <div class="card-header"><h3 class="card-title">{{$tagdata["nombre"]}}</h3></div>
         <div class="card-body">
         <p class="card-text">{{$tagdata["descripcion"]}}</p>
-        <p class="card-text">Vacio: 
+        <p class="card-text"><strong>Vacio:</strong>  
             @if ($tagdata["vacio"] == true)
                 true
             @else
                 false
             @endif
         </p>
-        <p class="card-text">Ejemplo: {{$tagdata["ejemplo"][0]}}</p>
+        @foreach ($tagdata["ejemplo"] as $item => $valor)
+             <p class="card-text"><strong>Ejemplo:</strong> {{$tagdata["ejemplo"][$item]}}</p>
+        @endforeach
+       
         </div>
         
     </section>
@@ -25,19 +28,24 @@
         <div class="card-header"><h3 class="card-title"># Atributos</h3></div>
         <div class="card-body">
         @foreach ($tagdata["atributo"] as $clave => $valor)
-            <h3 class="card-title">{{$clave}}</h3>
+            <h3 class="card-title"><strong>{{$clave}}</strong></h3>
             @foreach ($tagdata["atributo"][$clave] as $subclave => $subvalor)
             
             @if (is_array($tagdata["atributo"][$clave][$subclave]))
-                <p>Valores:</p>
+                <p><strong>Admite:</strong></p>
+                @php
+                    $i = 0;
+                @endphp
                 @foreach ($tagdata["atributo"][$clave][$subclave] as $subclave2 => $subvalor2)
+                
                     @if ($tagdata["atributo"][$clave][$subclave] == $tagdata["atributo"][$clave]["ejemplo"])
-                    <p class="card-text">Ejemplo: {{$tagdata["atributo"][$clave]["ejemplo"][0]}}</p>
+                        
+                    <p class="card-text"><strong>Ejemplo:</strong>  {{$tagdata["atributo"][$clave]["ejemplo"][$i++]}}</p>
                     
                     @else
  
                     <ul>
-                        <li class="card-text">{{$subclave2}}: {{$subvalor2}}</li>
+                        <li class="card-text"><strong>{{$subclave2}}</strong>: {{$subvalor2}}</li>
                     </ul>
                     
                     @endif
@@ -46,14 +54,14 @@
                     
             @elseif (is_bool($tagdata["atributo"][$clave][$subclave]) && true)
                 
-                <p class="card-text">Booleano: true</p>
+                <p class="card-text"><strong>Booleano:</strong> true</p>
             
             @elseif (is_bool($tagdata["atributo"][$clave][$subclave]) && false)
                 
-                <p class="card-text">Booleano: false</p>
+                <p class="card-text"><strong>Booleano:</strong> false</p>
 
             @else
-                    <p class="card-text">{{$subclave}}: {{$subvalor}}</p>
+                    <p class="card-text"><strong>{{$subclave}}:</strong> {{$subvalor}}</p>
             @endif
                 
             @endforeach
@@ -67,9 +75,9 @@
     <section class="card-wrapper " >
         <a name="ejemplos"></a>
 
-        <div class="card-header">Ejemplo</div>
+        <div class="card-header"><h3 class="card-title">Descripcion de ejemplos</h3></div>
         <div class="card-body">
-        <h5 class="card-title">Descripcion de ejemplos</h5>
+        
         <p class="card-text"></p>
         
         </div>
