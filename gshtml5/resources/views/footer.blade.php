ecdabae3f8e9045d5773e212c6df5f140cbf7d1c
JuanH <92516277+jehrx@users.noreply.github.com>
2022-04-08 13:21:16 +0200

    logo secundario ajustes

--- gshtml5/resources/views/footer.blade.php
+++ gshtml5/resources/views/footer.blade.php
@@ -1,21 +1,8 @@
-      <div class="nivel1">
-
-        <div class="caja">
-            <h2 class="sobre_nosotros">Sobre nosotros</h2>
-            <p> Somos alumnos de 1º de DAW del centro ITEC en Antequera (Málaga)</p>
-            <p> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Commodi, nemo.</p>
-            <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore, nihil.</p>
-        </div>
-
-        <div class="caja">
-
-        <h2 class="contactos">
-         <a href="/nosotros">¿Quieres contactarnos?</a>
-        </h2>
-
-        </div>
-
-    </div>    
-    <div class="nivel2">
-        <small>&copy; Curso: 2021/2022 <b>1º DAW</b> | Todos los derechos reservados.</small>
+<div class="fondo bg-black">
+    <div class="container">
+        <p class="nav justify-content-center text-light">
+        Somos alumnos de 1º DAW del centro ITEC (Antequera, Málaga)
+        </p>
+        <p class="text-center bg-warning text-black">&copy; 2021-2022 Company, Inc</p>
     </div>
+</div>
