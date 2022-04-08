c7313e688292dc9ab3bfdecd04cd3bb6d1482dff
JuanH <92516277+jehrx@users.noreply.github.com>
2022-04-08 13:29:43 +0200

    logo secundario ajustes

--- gshtml5/resources/views/footer.blade.php
+++ gshtml5/resources/views/footer.blade.php
@@ -1,8 +1,8 @@
 <div class="fondo bg-black">
     <div class="container">
         <p class="nav justify-content-center text-light">
-        Somos alumnos de 1º DAW del centro ITEC (Antequera, Málaga)
+        Alumnos 1º DAW del centro ITEC (Antequera, Málaga)
         </p>
-        <p class="text-center bg-warning text-black">&copy; 2021-2022 Company, Inc</p>
+        <p class="text-center bg-warning text-black">&copy; 2021-2022 , CódigoMollete</p>
     </div>
 </div>
