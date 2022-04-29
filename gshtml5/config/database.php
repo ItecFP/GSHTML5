efcd2240c79900f7282766ad9aaff900c2035ab7
eplq <91148468+eplq@users.noreply.github.com>
2022-04-29 13:44:02 +0200

    ya funciona de nuevo la db

--- gshtml5/config/database.php
+++ gshtml5/config/database.php
@@ -95,7 +95,8 @@ return [
 
         'mongodb' => [
             'driver' => 'mongodb',
-            'dsn' => env('DB_URI', 'mongodb://localhost:27017'),
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '27017'),
             'database' => env('DB_DATABASE', 'gshtml5db'),
             'username' => env('DB_USERNAME', 'root'),
             'password' => env('DB_PASSWORD', 'root'),
