b36679c95fd43b437d5f954c43c6088b997f6b74
Ignacio Núñez <itec.profesor.inr@outlook.es>
2022-05-04 08:59:29 +0200

    Elimino la inclusión de presentación y categoriagrid

--- gshtml5/resources/views/index.blade.php
+++ gshtml5/resources/views/index.blade.php
@@ -5,7 +5,6 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Guía Suprema - HTML5</title>
-
     <link rel="stylesheet" href="/bootstrap-5.1.3-dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="/css/style.css">
     <link rel="stylesheet" href="/css/stylecat.css">
@@ -18,8 +17,9 @@
        @include('topbar')
     </header>
     <main class="row">
-            @include('presentacion')
+<!--            @include('presentacion')
             @include('categoriagrid')
+-->
     </main>
     <footer class="footer">
         @include('footer')
